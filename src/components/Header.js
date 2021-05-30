c3be424144f00299092399c8019d325c6361c386
SindhuRBagal <47137650+SindhuRBagal@users.noreply.github.com>
2021-05-30 22:38:00 +0530

    Header Complete

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Typed from "react-typed";
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Hey, Iam Sindhu Bagal</h1>
+                
+            
+            </div>
+        </div>
+    )
+}
+
+export default Header
